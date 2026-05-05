bc94c7d0d76d395f55207054c87163e992e2c32a
Srilatha-31 <srilathadarsi12@gmail.com>
2026-05-05 12:15:15 +0530

    debug db host

--- db.py
+++ db.py
@@ -1,11 +1,13 @@
 import os
-import mysql.connector
 
 def get_db_connection():
+    print("HOST:", os.getenv("MYSQLHOST"))
+    print("PORT:", os.getenv("MYSQLPORT"))
+
     return mysql.connector.connect(
         host=os.getenv("MYSQLHOST"),
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT", 3306))  # 👈 FIX HERE
+        port=int(os.getenv("MYSQLPORT", 3306))
     )
