cf5d712ac8ad37888ac4cf8208a77040c4ddbc1a
Srilatha-31 <srilathadarsi12@gmail.com>
2026-05-05 10:46:29 +0530

    updated db connection

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
+import os
 import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Srilatha@01",
-        database="task_manager"
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT"))
     )
